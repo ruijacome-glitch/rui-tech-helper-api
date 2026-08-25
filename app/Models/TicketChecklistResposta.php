a9262f2bcc5a93476ba3ab17db40eb7b6c331b2b
Rui Jacome <ruijacome@gmail.com>
2026-08-25 17:05:13 +0100

    fix: remove unused HasFactory trait from TicketChecklistResposta

--- app/Models/TicketChecklistResposta.php
+++ app/Models/TicketChecklistResposta.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TicketChecklistResposta extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'ticket_id',
         'item_chave',
